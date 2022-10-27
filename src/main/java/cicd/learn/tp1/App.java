eac93334fbd99488e0de7e7175335fe65d81778d
Matthieu Fontaine <matth76330@live.fr>
2022-10-27 15:31:03 +0200

    Conflict

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        // fix du bug
-=======
         // Salut je re modifie
->>>>>>> feature/003
         System.out.println( "CI/CD rocks!" );
         // Salut je modifie le code
     }
