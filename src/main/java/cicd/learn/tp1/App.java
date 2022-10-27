29950fb5769213dbfce8d3dc6efe9ecadb26ad32
KingTigrou <matth76330@live.fr>
2022-10-27 14:30:56 +0200

    update welcome message

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -4,10 +4,10 @@ package cicd.learn.tp1;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "CICD rocks!" );
     }
 }
